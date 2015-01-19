1624cda56c707b78c7b36822ae0ab0c26bb722e1
Srijan R Shetty <srijan.shetty@gmail.com>
2015-01-19 20:16:46 +0530

    print cp

--- threshold_vectors.r
+++ threshold_vectors.r
@@ -10,12 +10,14 @@ na.pim[na.pim$glucose == 0, "glucose"] = NA
 na.pim[na.pim$triceps == 0, "triceps"] = NA
 na.pim[na.pim$insulin == 0, "insulin"] = NA
 
-# ---------- INFORMATION ---------------
-fit = rpart(formula = class ~ pregnant + glucose + bp + triceps + insulin + bmi + diabetes + age, method="class", data=na.pim, parms = list(split="information"), control = rpart.control(xval = 5, minsplit = 20, cp = 0))
-plot(fit, uniform=TRUE, main="Information")
+# ---------- ENTROPY ---------------
+fit = rpart(formula = class ~ pregnant + glucose + bp + triceps + insulin + bmi + diabetes + age, method="class", data=na.pim, parms = list(split="information"), control = rpart.control(xval = 5, minsplit = 23, cp = 0))
+plot(fit, uniform=TRUE, main="Threshold on vectors - Entropy")
 text(fit, use.n=TRUE, all=TRUE, cex=0.7)
+printcp(fit)
 
 # ---------- GINI ---------------
-fit = rpart(formula = class ~ pregnant + glucose + bp + triceps + insulin + bmi + diabetes + age, method="class", data=na.pim, parms = list(split="gini"), control = rpart.control(xval = 5, minsplit = 20, cp = 0))
-plot(fit, uniform=TRUE, main="GINI")
+fit = rpart(formula = class ~ pregnant + glucose + bp + triceps + insulin + bmi + diabetes + age, method="class", data=na.pim, parms = list(split="gini"), control = rpart.control(xval = 5, minsplit = 23, cp = 0))
+plot(fit, uniform=TRUE, main="Threshold on vectors - Gini")
 text(fit, use.n=TRUE, all=TRUE, cex=0.7)
+printcp(fit)
